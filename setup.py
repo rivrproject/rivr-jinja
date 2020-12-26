d6d45df0ce9e7dc3955c9fffe2322cf49c05fa5e
Kyle Fuller <kyle@fuller.li>
2020-12-26 14:47:41 +0000

    fix: expose py.typed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rivr-jinja',
-    version='0.4.0',
+    version='0.4.1',
     description='rivr integration for using the Jinja template engine.',
     url='https://github.com/rivrproject/rivr-jinja',
     packages=find_packages(),
